6eff25b740fae7c33f80a891dea2a4a98bc437d5
DanielePilati <danielepdj@gmail.com>
2024-05-18 00:26:32 +0200

    [upd] snack 7 fix

--- snack-7/snack-7.js
+++ snack-7/snack-7.js
@@ -2,14 +2,15 @@
 //Scrivere un programma che stampi la tabellina del 2, fino al numero 1000.
 
 const factor = 2;
-let value = 0;
-let numCap = 1000;
+let value = factor;
+
+// Modificate poi il programma in modo che venga chiesto all'utente il numero massimo consentito,
+let numCap = Number(prompt("inserisci nummero massimo"));
 
 for (let i = 0; i < numCap; i++) {
   value = value + factor;
+  //e stampare tutta la tabellina del 2 fino al numero inserito.
   console.log(value);
 }
-
-// Modificate poi il programma in modo che venga chiesto all'utente il numero massimo consentito,
 
 //e stampare tutta la tabellina del 2 fino al numero inserito.
