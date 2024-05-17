09bc0a9c78c7c3ac19dc4996ed405423e9e6c8b9
DanielePilati <danielepdj@gmail.com>
2024-05-18 00:41:18 +0200

    [upd] fixed snack 10

--- snack-10/snack-10.js
+++ snack-10/snack-10.js
@@ -2,9 +2,15 @@
 //Scrivere un programma a cui venga chiesto all'utente di inserire numeri uno dopo l'altro.
 
 const arrayNum = [];
-for (let i = 0; i < arrayNum.length; i++) {
-  userNum = Number(prompt("inserisci numero"));
-  numbers.push(userNum);
+
+for (let i = 0; i < 1000; i++) {
+  let userNum = Number(prompt("inserisci numero"));
+  if (arrayNum.includes(userNum)) {
+    alert("hai scritto due volte lo stesso numero");
+    break;
+  } else {
+    arrayNum.push(userNum);
+  }
 }
 
 // Quando l'utente scriverà 2 volte consecutive lo stesso numero allora il programma dovrà terminare.
