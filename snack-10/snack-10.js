9c22a1008871359dbf0ef12720c2014dbbdbad53
DanielePilati <danielepdj@gmail.com>
2024-05-20 18:32:58 +0200

    [fix] removed array

--- snack-10/snack-10.js
+++ snack-10/snack-10.js
@@ -1,16 +1,13 @@
 "use strict";
 //Scrivere un programma a cui venga chiesto all'utente di inserire numeri uno dopo l'altro.
-
-const arrayNum = [];
-
-for (let i = 0; i < 1000; i++) {
+let num = null;
+// Quando l'utente scriverà 2 volte consecutive lo stesso numero
+for (let i = 0; i < 1; i++) {
   let userNum = Number(prompt("inserisci numero"));
-  // Quando l'utente scriverà 2 volte consecutive lo stesso numero
-  if (arrayNum.includes(userNum)) {
-    alert("hai scritto due volte lo stesso numero");
+  if (userNum === num) {
     break;
-    // allora il programma dovrà terminare.
-  } else {
-    arrayNum.push(userNum);
   }
+  num = userNum;
+  i--;
+  // allora il programma dovrà terminare.
 }
